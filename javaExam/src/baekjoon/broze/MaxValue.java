093cf91af4dd32ab16c70d0f98b960bd19b76e8e
beargame123 <hoyoung7827@gmail.com>
2022-07-20 23:35:31 +0900

    .

---
+++ javaExam/src/baekjoon/broze/MaxValue.java
@@ -0,0 +1,33 @@
+package baekjoon.broze;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MaxValue {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        List<Integer> arr = new ArrayList<>();
+
+        for(int i =0; i<9; i++){
+            arr.add(in.nextInt());
+        }
+
+        int count = 0;
+        int max = 0;
+        int i = 0;
+
+        for(int value : arr) {
+            count++;
+
+            if(value > max) {
+                max = value;
+                i = count;
+            }
+        }
+        System.out.print(max + "\n" + i);
+
+    }
+}
