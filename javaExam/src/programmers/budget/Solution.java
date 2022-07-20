94ae5e5735ca98ef997a7d3d619c70d6bc4356c7
beargame123 <hoyoung7827@gmail.com>
2022-07-20 23:35:43 +0900

    .

--- javaExam/src/programmers/budget/Solution.java
+++ javaExam/src/programmers/budget/Solution.java
@@ -3,6 +3,7 @@ package programmers.budget;
 import java.util.Arrays;
 
 public class Solution {
+    // 프로그래머스 예산 문제입니다.
     public int solution(int[] d, int budget) {
         int answer = 0;
         int result = 0;
