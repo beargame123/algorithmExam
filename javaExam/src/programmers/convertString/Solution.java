d041644a4e22d74fb0513f4189b91f054b7c1b72
beargame123 <hoyoung7827@gmail.com>
2022-09-20 13:20:17 +0900

    ➕:: 프로그래머스 연습문제

---
+++ javaExam/src/programmers/convertString/Solution.java
@@ -0,0 +1,8 @@
+package programmers.convertString;
+
+public class Solution {
+    public int solution(String s) {
+        // 프로그래머스 문자열을 정수로 바꾸기 문제입니다. 번호 : 12925
+        return Integer.parseInt(s);
+    }
+}
