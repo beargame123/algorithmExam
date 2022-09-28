89911c53b214f5863ccb323f42fac2bddf779ef1
beargame123 <hoyoung7827@gmail.com>
2022-09-28 18:57:30 +0900

    🛠️:: 번호 수정

--- javaExam/src/programmers/twoIntegers/Solution.java
+++ javaExam/src/programmers/twoIntegers/Solution.java
@@ -2,15 +2,12 @@ package programmers.twoIntegers;
 
 public class Solution {
     public long solution(long a, long b) {
-        // 프로그래머스 문자열을 정수로 바꾸기 문제입니다. 번호 : 12925
-        if(a==b) return a;
-        long c = Math.max(a, b);
-        long d = Math.min(a, b);
-        int e = 0;
+        // 프로그래머스 두 정수 사이의 합 문제입니다. 번호 : 12912
+        long answer = 0;
 
-        for(long i = c; i>=d; i--){
-            e += i;
+        for(long i = (Math.min(a, b)); i<=(Math.max(a, b)); i++){
+            answer += i;
         }
-        return e;
+        return answer;
     }
 }
